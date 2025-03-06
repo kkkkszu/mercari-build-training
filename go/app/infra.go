e1037afa08f10449835207bf6d2dc8fe490d1f5e
kkngsz <30k.suzung@gmail.com>
2025-03-07 03:41:10 +0900

    step4-2 done

--- go/app/infra.go
+++ go/app/infra.go
@@ -2,7 +2,9 @@ package app
 
 import (
 	"context"
+        "encoding/json"
 	"errors"
+        "os"
 	// STEP 5-1: uncomment this line
 	// _ "github.com/mattn/go-sqlite3"
 )
@@ -12,6 +14,8 @@ var errImageNotFound = errors.New("image not found")
 type Item struct {
 	ID   int    `db:"id" json:"-"`
 	Name string `db:"name" json:"name"`
+        Category string `db:"Category" json:"Category"`
+        Image string `db:"image" json:"image"`
 }
 
 // Please run `go generate ./...` to generate the mock implementation
@@ -36,6 +40,36 @@ func NewItemRepository() ItemRepository {
 // Insert inserts an item into the repository.
 func (i *itemRepository) Insert(ctx context.Context, item *Item) error {
 	// STEP 4-2: add an implementation to store an item
+        data, err := os.ReadFile(i.fileName)
+    if err != nil && !os.IsNotExist(err) {
+        return err 
+    }
+
+    var items struct {
+        Items []Item `json:"items"` 
+    }
+
+    // 既存のデータがある場合
+    if len(data) > 0 {
+        err = json.Unmarshal(data, &items)
+        if err != nil {
+            return err 
+        }
+    }
+
+    items.Items = append(items.Items, *item)
+
+
+    updatedData, err := json.MarshalIndent(items, "", "  ")
+    if err != nil {
+        return err
+    }
+
+
+    err = os.WriteFile(i.fileName, updatedData, 0644)
+    if err != nil {
+        return err
+    }
 
 	return nil
 }
